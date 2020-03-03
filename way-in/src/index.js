cfbe5a9827da2fea6ec303c024cc8d3df798990a
ethoyer <hoyerelisabet@gmail.com>
2020-03-03 11:12:41 +0100

    set-up

--- way-in/src/index.js
+++ way-in/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Header extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        return (
+          <header>
+              <h1>Way In</h1>
+          </header>
+        );
+      }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+  );
