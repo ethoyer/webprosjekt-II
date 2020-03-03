fd42de47a5a2702a80c1fe58ee93c73b57fe5250
ethoyer <hoyerelisabet@gmail.com>
2020-03-03 12:11:34 +0100

    upd8

--- way-in/src/index.js
+++ way-in/src/index.js
@@ -7,17 +7,53 @@ class Header extends React.Component {
     render() {
         return (
           <header>
+              <img id="logo" src=""></img>
               <h1>Way In</h1>
           </header>
         );
+      }
+  }
+
+  class Indexbutton extends React.Component {
+
+    render() {
+        return (
+          <a href=""><div>
+              <img src=""></img>
+              <h2>Would you like to work with our students?</h2>
+              <p>Text text text text text</p>
+          </div></a>
+        );
+      }
+  }
+
+  class Footer extends React.Component {
+
+    render() {
+        return (
+          <footer>
+              <a href="">this is a link</a>
+              <a href="">log in</a>
+          </footer>
+        );
+      }
+  }
+
+  class Content extends React.Component {
+      render (){
+          return(
+              <div>
+                  <Header />
+                  <Indexbutton />
+                  <Footer />
+              </div>
+          );
       }
   }
   
   // ========================================
   
   ReactDOM.render(
-    <Header />,
+    <Content />,
     document.getElementById('root')
   );
-
-  //HELOOOOOOOOOOOOOOOO!
