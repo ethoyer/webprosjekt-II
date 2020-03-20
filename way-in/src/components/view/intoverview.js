ea53b3cdfc726a0b0a3b8f804c09c607b8c510cf
ethoyer <hoyerelisabet@gmail.com>
2020-03-20 10:21:05 +0100

    added projectArray

--- way-in/src/components/view/intoverview.js
+++ way-in/src/components/view/intoverview.js
@@ -9,17 +9,23 @@ import ProjectPreview from '../projectPreview';
 
 class IntOverview extends React.Component{
     render() {
+      let projectArray = [];
+
+      {internData.map((project)=>{
+        projectArray.push(project);
+      })}
+      
         return(
         <div>
     <Header />
     <main id="studAppMain">
       <h1 id="title">Apply Internship</h1>
       <div id="overview">
-    {internData.map((postDetail)=>{
+    {projectArray.map((postDetail)=>{
       return <ProjectPreview key={postDetail.id} project={postDetail} />
     })}
     </div>
-    <ProjectInfo />
+    <ProjectInfo array={projectArray} />
     </main>
     <Footer />
   </div>
