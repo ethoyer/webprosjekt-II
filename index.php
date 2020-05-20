4ae2caf32dc24ecfddc93e10d1e91962d81b9577
Isabell <isabell_h_j@hotmail.com>
2020-05-20 12:06:48 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$type = $_GET['tp'];
+if ($type == 'login') login();
+elseif ($type == 'companies') showCompanies();
+function login()
+{
+    require 'config.php';
+    $json = json_decode(file_get_contents('php://input'), true);
+    $username = $json['username'];
+    $password = $json['password'];
+    $userData = '';
+    $query = "select * from users where username='$username' and password='$password'";
+    $result = $db->query($query);
+    $rowCount = $result->num_rows;
+
+    if ($rowCount > 0) {
+        $userData = $result->fetch_object();
+        $userData = json_encode($userData);
+        echo '{"userData":' . $userData . '}';
+    } else {
+        echo '{"error":"Wrong username and password"}';
+    }
+}
+
+function showCompanies()
+{
+    require 'config.php';
+    $query = "SELECT * FROM feed ";
+    $result = $db->query($query);
+
+    $companyData = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $companyData = json_encode($companyData);
+
+    echo '{"companyData":' . $companyData . '}';
+}
