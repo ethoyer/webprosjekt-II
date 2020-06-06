a3d619e44e7f717f7670464b604497935f03d21a
elisabet <hoyerelisabet@gmail.com>
2020-06-06 14:31:46 +0200

    deliverable zip

--- way-in/way_in_db/config.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-header("Access-Control-Allow-Origin: *"); //allows access to data
-header("Access-Control-Allow-Headers: *");
-$db = new mysqli("localhost","root","","way_in");
-if(!$db) die("database connection error");
-?>
